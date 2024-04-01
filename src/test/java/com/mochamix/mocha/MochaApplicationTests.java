a2896ddad47be9560cd6bc3f094652706dae27c0
Yakxa <gui.lherme.brunetto@hotmail.com>
2024-04-01 18:51:03 -0300

    initial commit

---
+++ src/test/java/com/mochamix/mocha/MochaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochamix.mocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
